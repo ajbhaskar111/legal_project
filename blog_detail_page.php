e10c6158dec483978641f354f2e339d759c71b14
ajbhaskar111 <ajbhaskar111@gmail.com>
2024-06-17 12:12:28 +0530

    blog detail page

---
+++ blog_detail_page.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/style.css" />
+        <link rel="stylesheet" href="assets/fonts/stylesheet.css" />
+        <link rel="stylesheet" href="assets/icon_font/css/all.css" />
+        <link rel="stylesheet" href="assets/css/slick.css" />
+    </head>
+    <body>
+        <?php include 'include/header.php'; ?>
+        <section class="inner_page_slider position-relative">
+            <div class="" data-bs-interval="10000">
+                <img src="assets/media/inner_page_back.png" class="d-block w-100" alt="assets/media/banner.png" />
+                <div class="carousel-caption d-none d-md-block text-white">
+                    <h1 class="fs-45 mb-5 fw-bolder mb-0 text-uppercase">Agreement to sale mou</h1>
+                </div>
+            </div>
+        </section>
+        <section class="py-4 bg-black">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-8 col-sm-12 ">
+                        <div class="card position-relative shadow border-0">
+                            <div class="row align-items-center">
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <p class="primary_color fs-14 mb-0 p-3">May 16, 2023</p>
+                                </div>
+                            </div>
+                            <img src="assets/media/blog_img.png" class="card-img-top" alt="..." />
+                            <div class="card-body white_box">
+                                <div class="detail_text mb-5">
+                                    <h5 class="primary_color mb-4 mt-3 three_line_show">What is an Agreement to Sell?</h5>
+                                    <p class="card-text fs-15 text-justify show_detail_five_line black_text mb-4">
+                                    An agreement to sell is a contract between a seller and a buyer in which the seller agrees to sell property to the buyer at a specified price. It is generally used to sell immovable property such as land or buildings. The Agreement to Sell includes all the terms and conditions agreed upon by both parties. 
+                                    </p>
+                                </div>
+                                <div class="detail_text mb-5">
+                                    <h5 class="primary_color mb-4 mt-3 three_line_show">What is importance of Agreement to Sell?</h5>
+                                    <p class="card-text fs-15 text-justify show_detail_five_line black_text mb-4">
+                                    An Agreement to Sell a contract is important because it legally binds the parties involved in the sale of a property. This contract outlines the terms and conditions of the sale, including the price, date of sale, etc. The agreement protects parties from fraud or misrepresentation by clearly stating all material facts about the property. Further, it can be used as evidence in court if there are any disputes between the parties.
+                                    </p>
+                                </div>
+                                <div class="detail_text mb-5">
+                                    <h5 class="primary_color mb-4 mt-3 three_line_show">Stamp Duty on an Agreement to Sell</h5>
+                                    <p class="card-text fs-15 text-justify show_detail_five_line black_text mb-4">
+                                        In India, the buyer is liable to pay stamp duty whenever any immovable property is transferred. The amount of stamp duty payable depends on the state in which the property is situated as well as the value of the property.
+                                    </p>
+                                    <p class="card-text fs-15 text-justify show_detail_five_line black_text mb-4">
+                                    Similarly, when an agreement to sell is executed, stamp duty is also payable on it. The amount of stamp duty payable on an agreement to sell depends on various factors such as the property’s value, the nature of the transaction (sale/purchase/exchange/lease), etc.
+                                    </p>
+                                    <p class="card-text fs-15 text-justify show_detail_five_line black_text mb-4">
+                                    The party who executes the agreement to sell is liable to pay stamp duty. In most cases, the buyer pays stamp duty on an Agreement to Sell. 
+                                    </p>
+                                </div>
+                                <div class="detail_text mb-5 ">
+                                    <h5 class="primary_color mb-4 mt-3 three_line_show">Why is It Important to Perform a Property Title Verification before purchase any Property?</h5>
+                                    <ul class="listing hadding_list">
+                                        <li><a href="" class="fs-15 black_text text-decoration-none">Wide Spectrum of Clients</a></li>
+                                        <li><a href="" class="fs-15 black_text text-decoration-none">Wide Spectrum of Clients</a></li>
+                                        <li><a href="" class="fs-15 black_text text-decoration-none">Wide Spectrum of Clients</a></li>
+                                        <li><a href="" class="fs-15 black_text text-decoration-none">Wide Spectrum of Clients</a></li>
+                                    </ul>
+                                </div>
+                                <div class="detail_text mb-5 ">
+                                    <h5 class="primary_color mb-4 mt-3 three_line_show">Why is It Important to Perform a Property Title Verification before purchase any Property?</h5>
+                                    <div class="accordion" id="accordionExample">
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header">
+                                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                    Accordion Item #1
+                                                </button>
+                                            </h2>
+                                            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes
+                                                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth
+                                                    noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header">
+                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                    Accordion Item #2
+                                                </button>
+                                            </h2>
+                                            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes
+                                                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth
+                                                    noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header">
+                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                    Accordion Item #3
+                                                </button>
+                                            </h2>
+                                            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes
+                                                    control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth
+                                                    noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-12">
+                        <div class="card position-relative shadow border-0">
+                            <div class="">
+                                <form class="row p-md-5 aling-align-items-center">
+                                <h4 class="card-title fw-700 pt-4 text-center mb-4">Contact with us before you Buy your Property</h4>
+                                    <div class="col-md-12 mb-4">
+                                        <input type="text" class="form-control" id="inputEmail4" placeholder="Enter your Full name">
+                                    </div>
+                                    <div class="col-md-12 mb-4">
+                                        <input type="text" class="form-control" id="inputEmail4" placeholder="Enter your Contact">
+                                    </div>
+                                    <div class="col-12 mb-4">
+                                        <input type="email" class="form-control" id="inputEmail4" placeholder="Enter your Contact">
+                                    </div>
+                                    <div class="col-12 mb-4">
+                                        <textarea name="" id="" rows="10" class="form-control w-100" placeholder="Message"></textarea>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <button type="submit" class="btn site_black_btn primary_color text-capitalize fs-15 bg_site_black fw-700 w-100">submit request</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php include 'include/footer.php'; ?>
+        <script src="assets/vendor/popper.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script src="assets/vendor/slick.min.js"></script>
+    </body>
+</html>
